74eabd2de2a3dd095e8d327b21ec06ab844bd1c9
Dibendu Kundu <dibendukundu5@gmail.com>
2021-02-11 00:52:54 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCE_BlqiWxFbduOnTz49uigzQgtjyKnWCU',
+  authDomain: 'chat-web-app-d4833.firebaseapp.com',
+  projectId: 'chat-web-app-d4833',
+  storageBucket: 'chat-web-app-d4833.appspot.com',
+  messagingSenderId: '809829012028',
+  appId: '1:809829012028:web:d105a2172fcfb98a006939',
+};
+
+const app = firebase.initializeApp(config);
